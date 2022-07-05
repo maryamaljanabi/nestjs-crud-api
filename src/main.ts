07af213f5f047ff308018985bfa92736bd5bb7b1
maryamaljanabi <maryam.aljanabi135@gmail.com>
2022-07-05 21:45:18 +0300

    feat: Add Security Middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // security middlewares
+  app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
+
   await app.listen(3000);
 }
 bootstrap();
