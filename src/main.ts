c4e19279d2f1100260e388295bedd67312933f20
maryamaljanabi <maryam.aljanabi135@gmail.com>
2022-07-09 13:48:22 +0300

    refactor: Move the Application Port to .env

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 5000);
 }
 bootstrap();
